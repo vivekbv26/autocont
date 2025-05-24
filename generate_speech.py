614f0324f8e02de956daab4a7e60f5e8970b701c
unknown <rishi2004vishu@gmail.com>
2025-05-24 22:00:25 +0530

    speech model

--- generate_speech.py
+++ generate_speech.py
@@ -5,8 +5,6 @@ from elevenlabs import play
 
 def generate_speech(text, voice_id,output_path="output/output.mp3"):
     ELEVENLABS_API_KEY = get_env_var("ELEVENLABS_API_KEY")
-    ELEVENLABS_VOICE_ID = get_env_var("ELEVENLABS_VOICE_ID")
-
     client = ElevenLabs(
     api_key=ELEVENLABS_API_KEY,
     )        
@@ -16,11 +14,12 @@ def generate_speech(text, voice_id,output_path="output/output.mp3"):
         text=text,
         model_id="eleven_multilingual_v2"
     )
-    play(audio)
-    # response = requests.post("https://api.elevenlabs.io/v1/text-to-speech/JBFqnCBsd6RMkjVDRZzb?output_format=mp3_44100_128")
+    audio_bytes = b"".join(audio)
+
+    play(audio_bytes)
+
     with open(output_path, "wb") as f:
-        f.write(audio)
-    return output_path
+        f.write(audio_bytes)
 def main():
     generate_speech(text="Hello, Vicky how does this sound, I think it sounds pretty good", voice_id="JBFqnCBsd6RMkjVDRZzb")
 if __name__ == "__main__":
