350570b044f6fe3cf26f8f244f20e214ff55e23a
unknown <rishi2004vishu@gmail.com>
2025-05-25 18:32:49 +0530

    video text

--- generate_video.py
+++ generate_video.py
@@ -2,77 +2,16 @@ import requests
 import json
 from utils import get_env_var
 import time
-
-
-def generate(input_path="output/output1.mp3"):
-    HEYGEN_API_KEY = get_env_var("HEYGEN_API_KEY")
-    url = "https://upload.heygen.com/v1/asset"
-
-    headers = {
-        "Content-Type": "audio/mpeg",
-        "X-Api-Key": HEYGEN_API_KEY,
-    }
-
-    file_path = input_path
-
-    with open(file_path, "rb") as file:
-        response = requests.post(url, headers=headers, data=file)
-    print(response.status_code)
-    print(response.json().get("data")['id'])
-    return generate_video(response.json().get("data")['id'], title="Test Video", dimension="16:9")
-
-
-def generate_video(audio_asset_id,title="try",dimension="16:9"):
-    HEYGEN_API_KEY = get_env_var("HEYGEN_API_KEY")
-    url = "https://api.heygen.com/v2/video/generate"
-    headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "x-api-key": HEYGEN_API_KEY
-    }
-    payload = {
-        "title": title,
-        "caption": False,
-         "dimension": {
-            "width": 1280,
-            "height": 720
-        },
-        "video_inputs": [
-            {
-                "character": {
-                    "type": "avatar",
-                    "avatar_id":"Conrad_sitting_sofa_front",
-                    "scale": 1.0,
-                    "avatar_style": "normal",
-                    "offset": {
-                        "x": 0.0,
-                        "y": 0.0
-                    }
-                },
-                "voice": {
-                    "type": "audio",
-                    "audio_asset_id": audio_asset_id
-                },
-                "background": {
-                    "type": "color",
-                    "value": "#f6f6fc"
-                }
-            }
-        ]
-    }
-    response=requests.post(url,headers=headers,data=json.dumps(payload))
-    if response.status_code==200:
-        result = response.json()
-        print(result)
-        print(f"Video request successful. Video ID: {result.get('data')['video_id']}")
-        return (result.get('data')['video_id'])
-    else:
-        print("Error creating video:", response.status_code, response.text)
-        return None
-
+from generate_video_audio import generate
+from generate_video_text import generate_video_text
 def main():
     HEYGEN_API_KEY = get_env_var("HEYGEN_API_KEY")
-    video_id = generate(input_path="output/output.mp3")
+    a=0
+    text = "Former President Donald Trump delivered a politically charged commencement address to the West Point Class of 2025, criticizing military diversity initiatives and touting his administration's defense policies. He emphasized the rollback of DEI programs and political training, highlighting his efforts to strengthen the military."
+    if(a==1):
+        video_id = generate(input_path="output/output1.mp3")
+    else:
+        video_id = generate_video_text(text=text, title="try")
     print("Video ID:", video_id)
     if not video_id:
         print("Video ID not returned.")
